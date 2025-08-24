37543373837c022e2ecda22ab6ecf0420514e271
ayushdeo <mailbox4ayushxxx@gmail.com>
2025-08-24 16:32:36 -0700

    backend: allow CORS from render static site

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,21 +1,30 @@
+# app/main.py
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
+
 from app.api.campaigns import router as campaigns_router
 from app.api.campaign_store import campaign_store_router
+from app.api.email_hub import router as emailhub_router
 
 app = FastAPI(title="Connecttr Backend")
 
-# allow the React app to call the API locally
+# Allow your deployed frontend + local dev
+FRONTEND_ORIGINS = [
+    "http://localhost:3000",
+    "https://connecttr-front-end.onrender.com",
+    os.getenv("FRONTEND_ORIGIN", "").rstrip("/"),   # optional extra
+]
+ALLOWED_ORIGINS = [o for o in FRONTEND_ORIGINS if o]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=ALLOWED_ORIGINS,
+    allow_credentials=True,        # ok because we’re not using "*"
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True,
 )
 
 app.include_router(campaigns_router)
 app.include_router(campaign_store_router)
-
-from app.api.email_hub import router as emailhub_router
 app.include_router(emailhub_router)
