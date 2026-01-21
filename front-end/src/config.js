1f95fb3ce4701895ce00a6b566799c1e0f6e3b3f
ayushdeo <mailbox4ayushxxx@gmail.com>
2026-01-21 12:42:03 -0800

    fix(frontend): remove hardcoded localhost and use REACT_APP_API_BASE

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,7 +1,9 @@
-export const API_BASE =
-  process.env.REACT_APP_API_URL || "http://localhost:8000";
+export const API_BASE = process.env.REACT_APP_API_BASE;
+
+if (!API_BASE) {
+  console.warn("REACT_APP_API_BASE is not defined. API calls may fail.");
+}
 
 export const API_BASE_URL = API_BASE;
-
-export const API = process.env.REACT_APP_API_BASE || API_BASE;
+export const API = API_BASE;
 
